5447cf990e0c1410537143d31a6298501a0f9de9
llocr <36-96@naver.com>
2023-06-04 20:27:31 +0900

    1. JDBCTemplate.java resource 경로 찾지 못하는 문제 해결 (뭉) 2. 전체 회원 조회 구현 완료... :)

--- src/main/java/com/common/jdbc/JDBCTemplate.java
+++ src/main/java/com/common/jdbc/JDBCTemplate.java
@@ -7,14 +7,13 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
 
 public class JDBCTemplate {
     private static SqlSessionFactory sqlSessionFactory;
 
     public static SqlSession getSqlSession() {
         if(sqlSessionFactory == null) {
-            String resource = "webapp/WEB-INF/config/mybatis-config.xml";
+            String resource = "config/mybatis-config.xml";
             try {
                 InputStream inputStream = Resources.getResourceAsStream(resource);
                 sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
